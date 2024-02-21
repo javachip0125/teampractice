83aea1d73e8c8ccfeae83cf65813b17d387680f7
Bini0104 <goqsldl0120@gmail.com>
2024-02-21 11:10:11 +0900

    commit

--- src/main/java/Calculator/multiply/multiply.java
+++ src/main/java/Calculator/multiply/multiply.java
@@ -1,4 +1,17 @@
 package Calculator.multiply;
 
+import java.util.Scanner;
+
 public class multiply {
+    public int mult(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("수를 입력해주세요. : ");
+        int num1 = sc.nextInt();
+        System.out.print("곱할 수를 입력해주세요. : ");
+        int num2 = sc.nextInt();
+
+        return num1 * num2;
+    }
 }
